5e2dde3740d47ead5d9286c7d5601e3a9a9b1729
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-01-23 17:13:24 +0100

    basic theaming getting some data for testing

--- modules/inat_obs/theme/observations.tpl.php
+++ modules/inat_obs/theme/observations.tpl.php
@@ -1,6 +1,22 @@
 <?php
-print('adsf');
-//var_dump($content);
+/**
+ * iNaturalist theme for observations
+ * Copyright 2014 Projecte Ictineo SCCL (www.projecteictineo.com)
+ * aGPLv3
+ *
+ * All data is recived in $content variable
+ * the structure is like is retoruned by
+ * inaturalist api
+ */
 dsm($content);
+
+foreach( $content as $id => $obs) {
+  print('<div class="obs-wrapper '. $id .'" id="obs_'.$id.'">');
+  print('  <h2>'.$obs['species_guess'].'</h2>');
+  print('  <img src="'.$obs['photos'][0]['medium_url'].'" />');
+  print('</div>');
+
+}
+
+
 ?>
-<h3> HELLO WORLD </h3>
