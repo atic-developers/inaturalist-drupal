6ce3f14c4e90d6e59677002090e79b8e07ab63ac
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-01-24 11:06:08 +0100

    some theme progress, subtheme for observations and sass files created

--- modules/inat_obs/theme/observations.tpl.php
+++ modules/inat_obs/theme/observations.tpl.php
@@ -8,15 +8,12 @@
  * the structure is like is retoruned by
  * inaturalist api
  */
-dsm($content);
-
-foreach( $content as $id => $obs) {
-  print('<div class="obs-wrapper '. $id .'" id="obs_'.$id.'">');
-  print('  <h2>'.$obs['species_guess'].'</h2>');
-  print('  <img src="'.$obs['photos'][0]['medium_url'].'" />');
-  print('</div>');
+foreach( $observations as $id => $obs) {
+  print($obs);
 
 }
 
 
 ?>
+
+
