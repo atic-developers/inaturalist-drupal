9bc454dde9437f10fb986661e32cb01c83112228
Joangi <joan@projecteictineo.com>
2014-01-27 12:33:45 +0100

    HTTP petiton realesed

---
+++ modules/inat_obs/theme/map.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * iNaturalist theme for observations map
+ * Copyright 2014 Projecte Ictineo SCCL (www.projecteictineo.com)
+ * aGPLv3
+ *
+ * All data is recived in $content variable
+ * the structure is like is retoruned by
+ * inaturalist api
+ */
+
+/**
+ * Add mapping bases
+ */
+
+foreach( $observations as $id => $obs) {
+  print($obs);
+}
+
+
+?>
+
+
