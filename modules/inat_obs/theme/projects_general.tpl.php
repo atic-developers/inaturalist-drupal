7a53a5fc1b112654483518d5c402d9b7d0a05666
Joangi <joan@projecteictineo.com>
2014-04-25 09:23:43 +0200

    Llistat de projectes realized

---
+++ modules/inat_obs/theme/projects_general.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * iNaturalist theme for observations
+ * Copyright 2014 Projecte Ictineo SCCL (www.projecteictineo.com)
+ * aGPLv3
+ *
+ * All data is recived in $content variable
+ * the structure is like is retoruned by
+ * inaturalist api
+ */
+
+/**
+ * Add mapping bases
+ */
+foreach( $projects as $id => $obs) {
+  print($obs);
+}
+
+
+?>
+
+
---
+++ modules/inat_obs/theme/projects_list.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * iNaturalist theme for observations
+ * Copyright 2014 Projecte Ictineo SCCL (www.projecteictineo.com)
+ * aGPLv3
+ *
+ * All data is recived in $content variable
+ * the structure is like the retoruned by
+ * inaturalist api
+ */
+//dsm($observation);
+global $base_url;
+?>
+<div class="inat_project row" id="prj_<?php print($projects['id']); ?>">
+  <div class="photo">
+      <img src="<?php print($projects['icon_url']); ?>"/>
+  </div> <!-- /photo -->
+  <h2><a href="<?php print($base_url . '/inat/projects/' . $projects['id']); ?>"><?php print($projects['title']); ?></a></h2>
+  <div class="description"><?php print($projects['description']); ?></div>
+
+</div>
