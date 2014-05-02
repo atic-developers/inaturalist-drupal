f078971a61a8d1b1625e35342db59a8e6074ca7c
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-05-02 12:15:14 +0200

    gestio de featured i pagines en el template wrapper del llistat

--- modules/inat_obs/theme/projects_general.tpl.php
+++ modules/inat_obs/theme/projects_general.tpl.php
@@ -9,14 +9,55 @@
  * inaturalist api
  */
 
-/**
- * Add mapping bases
- */
-print('<a href="#"> switch featured </a>');
+global $base_url;
+
+$query = explode('/',current_path());
+$isfeatured = FALSE;
+$has_prev = FALSE;
+dsm($query);
+if(!isset($query[2])) {
+  $query[2] = 'featured';
+}elseif(is_numeric($query[2])) {
+  //$query[3] = $query[2];
+  $query[2] = 'featured';
+}elseif($query[2] == 'featured') {
+  $query[2] = 'all';
+  $isfeatured = TRUE;
+}else{
+  $query[2] = 'featured';
+}
+$featured_url = url(implode('/',$query));
+$query = explode('/',current_path());
+$query_prev = $query;
+$query_next = $query_prev;
+if(!isset($query[2]) || isset($query[2]) && $query[2] == 0) {
+  $query_next[2] = '2';
+  $query_prev[2] = '1';
+}elseif(is_numeric($query[2])) {
+  $query_next[2] += 1;
+  $query_prev[2] -= 1;
+  $has_prev = TRUE;
+}elseif(!isset($query[3]) || isset($query[3]) && $query[3] == 0){
+  $query_next[3] = '2';
+  $query_prev[3] = '1';
+}elseif(is_numeric($query[3])){
+  $query_next[3] += 1;
+  $query_prev[3] -= 1;
+  $has_prev = TRUE;
+}
+$next_url = url(implode('/',$query_next));
+$prev_url = url(implode('/',$query_prev));
+
+if($isfeatured) {
+  print('<a href="'.$featured_url.'">'.t('Show all').'</a>');
+} else {
+  print('<a href="'.$featured_url.'">'.t('Show featured').'</a>');
+}
+//print('<h4>'.t('Showing page:').' </h4>');
 foreach( $projects as $id => $obs) {
   print($obs);
 }
-print('<a href="#"> prev </a><a href="#"> next </a>');
+print('<a href="'.$prev_url.'"> prev </a><a href="'.$next_url.'"> next </a>');
 
 ?>
 
