448c3a0cadeed8daae9a695a49dde8351dbc78fa
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-05-05 14:06:33 +0200

    added theme/taxa_list template

---
+++ modules/inat_obs/theme/taxa_list.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * iNaturalist theme for observations
+ * Copyright 2014 Projecte Ictineo SCCL (www.projecteictineo.com)
+ * aGPLv3
+ *
+ * All data is recived in $content variable
+ * the structure is like the retoruned by
+ * inaturalist api
+ */
+//dsm($observation);
+global $base_url;
+foreach($taxons as $id => $taxa):
+?>
+<div class="inat_taxa row row-<?php print($id); ?>" id="prj_<?php print($taxa['id']); ?>">
+  <div class="photo">
+      <img src="<?php print($taxa['photo_url']); ?>"/>
+  </div> <!-- /photo -->
+  <h2><a href="<?php print($base_url . '/inat/taxa/' . $taxa['id']); ?>"><?php print($taxa['name']); ?></a></h2>
+  <div class="description"><?php print($taxa['wikipedia_summary']); ?></div>
+
+</div>
+<?php endforeach; ?>
