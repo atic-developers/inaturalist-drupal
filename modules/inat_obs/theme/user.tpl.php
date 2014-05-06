d361f6f2f347e9f49da3bd94aa0f84a98fa861d5
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-05-06 11:14:54 +0200

    added basic user suport added files

---
+++ modules/inat_obs/theme/user.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * iNaturalist theme for observations
+ * Copyright 2014 Projecte Ictineo SCCL (www.projecteictineo.com)
+ * aGPLv3
+ *
+ * All data is recived in $content variable
+ * the structure is like the retoruned by
+ * inaturalist api
+ */
+//dsm($observation);
+global $base_url;
+dsm($user);
+?>
+<div class="inat_project row" id="prj_<?php print($user['id']); ?>">
+  <div class="photo">
+      <img src="<?php print($user['medium_user_icon_url']); ?>"/>
+  </div> <!-- /photo -->
+  <h2><a href="<?php print($base_url . '/inat/user/' . $user['id']); ?>"><?php print($user['name']); ?></a></h2>
+  <div class="description"><?php print($user['description']); ?></div>
+
+</div>
