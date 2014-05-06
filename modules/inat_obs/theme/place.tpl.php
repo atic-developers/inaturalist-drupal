e16cf45f6adf56b026183f0a4bf0531c5c1699f5
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-05-06 09:30:15 +0200

    added maps to place list and place view

--- modules/inat_obs/theme/place.tpl.php
+++ modules/inat_obs/theme/place.tpl.php
@@ -10,10 +10,25 @@
  */
 //dsm($observation);
 global $base_url;
+dsm($place);
 ?>
+ <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css" />
+ <script src="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.js"></script>
 <div class="inat_project row" id="prj_<?php print($place['id']); ?>">
   <div class="photo">
-      <img src="<?php print($place['photo_url']); ?>"/>
+    <div id="map-<?php print($place['id']); ?>" style="width: 400px; height: 400px;"></div>
+    <script type="text/javascript">
+      var map = L.map('map-<?php print($place['id']); ?>').setView([51.505, -0.09], 13);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        maxZoom: 18
+        }).addTo(map);
+      var bounds = new Array();
+      bounds.push(new Array([<?php print($place['nelat']);?>, <?php print($place['nelng']); ?>]));
+      bounds.push(new Array([<?php print($place['swlat']);?>, <?php print($place['swlng']); ?>]));
+      L.marker().setLatLng([<?php print($place['latitude']); ?>, <?php print($place['longitude']);?>]).addTo(map);
+      map.fitBounds(bounds);
+    </script>
+
   </div> <!-- /photo -->
   <h2><a href="<?php print($base_url . '/inat/place/' . $place['id']); ?>"><?php print($place['display_name']); ?></a></h2>
   <?php if($place['parent_id'] != ''): ?><a href="<?php print($base_url . '/inat/place/' . $place['parent_id']); ?>"><?php print(t('Parent')); ?></a><?php endif;?>
--- modules/inat_obs/theme/place_list.tpl.php
+++ modules/inat_obs/theme/place_list.tpl.php
@@ -18,13 +18,26 @@ if($current_page > 1) {
   $prev_url .= '1';
 }
 $next_url .= $current_page + 1;
-
-
+?>
+ <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css" />
+ <script src="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.js"></script>
+<?php
 foreach($places as $id => $place):
 ?>
 <div class="inat_place row row-<?php print($id); ?>" id="prj_<?php print($place['id']); ?>">
   <div class="photo">
-      <img src="<?php //print($place['photo_url']); ?>"/>
+    <div id="map-<?php print($place['id']); ?>" style="width: 150px; height: 150px;"></div>
+    <script type="text/javascript">
+      var map = L.map('map-<?php print($place['id']); ?>').setView([51.505, -0.09], 13);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        maxZoom: 18
+        }).addTo(map);
+      var bounds = new Array();
+      bounds.push(new Array([<?php print($place['nelat']);?>, <?php print($place['nelng']); ?>]));
+      bounds.push(new Array([<?php print($place['swlat']);?>, <?php print($place['swlng']); ?>]));
+      L.marker().setLatLng([<?php print($place['latitude']); ?>, <?php print($place['longitude']);?>]).addTo(map);
+      map.fitBounds(bounds);
+    </script>
   </div> <!-- /photo -->
   <h2><a href="<?php print($base_url . '/inat/place/' . $place['id']); ?>"><?php print($place['display_name']); ?></a></h2>
 
