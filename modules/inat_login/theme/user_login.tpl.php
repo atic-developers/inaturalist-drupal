2b690ed9f07f3f0f6c8c2fc974d3252e05d35a98
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-05-13 10:36:41 +0200

    basic user login support files added

---
+++ modules/inat_login/theme/user_login.tpl.php
@@ -0,0 +1,14 @@
+<?php
+if($login->user_icon_url == NULL) { $login['user_icon_url'] = variable_get('inat_base_url', 'http://www.inaturalist.org') . '/attachment_defaults/users/icons/defaults/thumb.png'; }
+// Template del modúl de login
+?>
+
+<div id='user-data'>  
+  <h4 id='login'> <?php print $login['login'] ?> </h4>
+  <div id='usr-img'>  <img src='<?php print $login['user_icon_url'] ?>'> </img></div>
+  <div id='profile'> <a href='<?php print $login['uri'] ?>'> Profile </a> </div> 
+</div> 
+
+
+<?
+?>
