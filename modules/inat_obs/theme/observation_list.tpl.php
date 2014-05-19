41ac64b834aa7330b36dc8a9184a2492e9fbb759
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-05-19 14:49:51 +0200

    multiple photo suport for obs list

--- modules/inat_obs/theme/observation_list.tpl.php
+++ modules/inat_obs/theme/observation_list.tpl.php
@@ -17,8 +17,17 @@ global $base_url;
     <?php if (array_key_exists('photos_count',$observation) && $observation['photos_count'] == 0): ?>
       <span class="no_photo"><?php print(t('No photo')); ?></span>
     <?php elseif(isset($observation['photos'][0])): ?>
-      <?php //TODO considere multiple photos ?>
-      <img src="<?php print($observation['photos'][0]['small_url']); ?>" alt="<?php print($observation['description']); ?>" />
+      <div class="cycle-slideshow  img-wrapper img-wrapper-<?php print $id; ?>"
+      data-cycle-slides="> figure"
+      data-cycle-fx=fade
+      >
+        <?php foreach($observation['photos'] as $id => $img): ?>
+          <figure>
+            <img src="<?php print($img['small_url']); ?>" alt="<?php print($observation['description']); ?>" class="img-<?php print $id; ?>"/>
+            <figurecaption><?php print($img['attribution']); ?></figurecaption>
+          </figure>
+        <?php endforeach; ?>
+      </div>
     <?php endif; ?>
   </div> <!-- /photo -->
   <h2><a href="<?php print($base_url . '/inat/observation/' . $observation['id']); ?>"><?php print($observation['species_guess']); ?></a></h2>
