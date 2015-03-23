3d323b5b52200132cc972cf0874c5bafee02fa33
Joangi <joan@projecteictineo.com>
2015-03-23 17:35:53 +0100

    list observation in a Transect

--- modules/inat_obs/theme/node--inat_transect.tpl.php
+++ modules/inat_obs/theme/node--inat_transect.tpl.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
@@ -30,7 +27,8 @@
 
   <?php print render($content['comments']); ?>
 
-</div>
+</div> 
+
 <?php //showing all observations associated with the transect
 $query = new EntityFieldQuery();
 $query->entityCondition('entity_type', 'node')
@@ -41,10 +39,53 @@ $query->entityCondition('entity_type', 'node')
   ->addMetaData('account', user_load(1)); // Run the query as user 1.
 
 $result = $query->execute();
-dsm($result);
 if (isset($result['node'])) {
  $news_items_nids = array_keys($result['node']);
  $news_items = entity_load('node', $news_items_nids);
 }
+ foreach ($news_items as $norender) {
+   $render = node_view($norender, $view_mode = 'teaser');
+   $rendered= drupal_render($render);
+   print ($rendered);
+}
 ?>
+<div class="inat_observation row" id="obs_<?php print($id); ?>">                                                                      
+  <div class="photo">
+    <?php if (array_key_exists('photos_count',$observation) && $observation['photos_count'] == 0): ?>  
+      <span class="no_photo"><?php print(t('No photo')); ?></span>
+    <?php elseif(isset($observation['photos'][0])): ?>
+      <div class="cycle-slideshow  img-wrapper img-wrapper-<?php print $id; ?>"
+      data-cycle-slides="> figure"
+      data-cycle-fx=fade
+      >
+        <?php foreach($observation['photos'] as $id => $img): ?>
+          <figure>
+            <img src="<?php print($img['small_url']); ?>" alt="<?php print($observation['description']); ?>" class="img-<?php print $id; ?>"/>
+            <figurecaption><?php print($img['attribution']); ?></figurecaption>
+          </figure>
+        <?php endforeach; ?>
+      </div>
+     <?php else: ?>       
+<?php print('<span class="no_photo"> <img src="'.$base_url.'/sites/all/modules/inat/modules/inat_obs/img/default.png"></img></span>'); ?>
+ 
+   <?php endif; ?>
+ </div> <!-- /photo -->
+  <h2><a href="<?php print($base_url . '/inat/observation/' . $observation['id']); ?>"><?php print($observation['species_guess']); ?></a></h2>
+  <div class="description"><?php print($observation['description']); ?></div>
+  
+ 
+  <?php if(isset($observation['user']['login'])): ?>
+     <div class="observer"><span class="label"><?php print(t('Observer: ') ."</span>". $observation['user']['login']); ?></div> 
+  <?php endif; ?>
 
+    <div class="date"><span class="label"><?php if(isset($observation['observed_on'])){ 
+        $d = DateTime::createFromFormat('Y-m-d', $observation['observed_on'])->format('l j F Y');
+        print(t('Date observed: ')."</span>".$d);
+        } ?>
+    </div>
+
+
+  <?php if(isset($observation['place_guess']) && $observation['place_guess'] != ''): ?>
+    <div class="place"><span class="label"><?php print(t('Place: ')."</span>".$observation['place_guess']); ?></div>
+  <?php endif; ?>
+</div>                      
