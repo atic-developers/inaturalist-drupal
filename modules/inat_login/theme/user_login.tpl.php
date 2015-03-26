10a8a37672cbb836012c6430fff9993ad5f201f6
Joangi <joan@projecteictineo.com>
2015-03-26 10:32:37 +0100

    Every observation added to inat, it's added to the preconfigured project

--- modules/inat_login/theme/user_login.tpl.php
+++ modules/inat_login/theme/user_login.tpl.php
@@ -3,11 +3,10 @@ global $base_url;
 if($login['user_icon_url'] == NULL) { $login['user_icon_url'] = variable_get('inat_base_url', 'http://www.inaturalist.org') . '/attachment_defaults/users/icons/defaults/thumb.png'; }
 // Template del modúl de login
 ?>
-
 <div id='user-data'>  
   <h4 id='login'> <?php print $login['login'] ?> </h4>
-  <div id='usr-img'>  <img src='<?php print $login['user_icon_url'] ?>'> </img></div>
-  <div id='profile'> <a href='<?php print $base_url . '/inat/user/'. $login['id'] ?>'> Profile </a>[<a href="<?php print $base_url . '/inat/edit/user/'. $login['id']; ?>" class="edit"><?php print t('Edit'); ?></a>] </div> 
+  <div id='usr-img'>  <img src='<?php print $login['user_icon_url'] ?>'> </img></div> 
+ <div id='profile'> <a href='<?php print ('user') print $base_url . '/inat/user/'. $login['id'] ?>'> Profile </a>[<a href="<?php print $base_url . '/inat/edit/user/'.'$login["id"]'; ?>" class="edit"><?php print t('Edit'); ?></a>] </div> 
   <div id='email'> <?php print $login['email'] ?> </div> 
   <div id='logout'> <a href='<?php print $base_url; ?>/inat/user/logout'> Logout </a> </div> 
 </div> 
--- modules/inat_obs/theme/node--inat_transect.tpl.php
+++ modules/inat_obs/theme/node--inat_transect.tpl.php
@@ -1,3 +1,7 @@
+
+<?php
+global $base_url; 
+ ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
@@ -49,43 +53,3 @@ if (isset($result['node'])) {
    print ($rendered);
 }
 ?>
-<div class="inat_observation row" id="obs_<?php print($id); ?>">                                                                      
-  <div class="photo">
-    <?php if (array_key_exists('photos_count',$observation) && $observation['photos_count'] == 0): ?>  
-      <span class="no_photo"><?php print(t('No photo')); ?></span>
-    <?php elseif(isset($observation['photos'][0])): ?>
-      <div class="cycle-slideshow  img-wrapper img-wrapper-<?php print $id; ?>"
-      data-cycle-slides="> figure"
-      data-cycle-fx=fade
-      >
-        <?php foreach($observation['photos'] as $id => $img): ?>
-          <figure>
-            <img src="<?php print($img['small_url']); ?>" alt="<?php print($observation['description']); ?>" class="img-<?php print $id; ?>"/>
-            <figurecaption><?php print($img['attribution']); ?></figurecaption>
-          </figure>
-        <?php endforeach; ?>
-      </div>
-     <?php else: ?>       
-<?php print('<span class="no_photo"> <img src="'.$base_url.'/sites/all/modules/inat/modules/inat_obs/img/default.png"></img></span>'); ?>
- 
-   <?php endif; ?>
- </div> <!-- /photo -->
-  <h2><a href="<?php print($base_url . '/inat/observation/' . $observation['id']); ?>"><?php print($observation['species_guess']); ?></a></h2>
-  <div class="description"><?php print($observation['description']); ?></div>
-  
- 
-  <?php if(isset($observation['user']['login'])): ?>
-     <div class="observer"><span class="label"><?php print(t('Observer: ') ."</span>". $observation['user']['login']); ?></div> 
-  <?php endif; ?>
-
-    <div class="date"><span class="label"><?php if(isset($observation['observed_on'])){ 
-        $d = DateTime::createFromFormat('Y-m-d', $observation['observed_on'])->format('l j F Y');
-        print(t('Date observed: ')."</span>".$d);
-        } ?>
-    </div>
-
-
-  <?php if(isset($observation['place_guess']) && $observation['place_guess'] != ''): ?>
-    <div class="place"><span class="label"><?php print(t('Place: ')."</span>".$observation['place_guess']); ?></div>
-  <?php endif; ?>
-</div>                      
