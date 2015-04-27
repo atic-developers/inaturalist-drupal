152c718775ac919f99c141fc6d83137104b327ac
Joangi <joan@projecteictineo.com>
2015-04-27 18:07:45 +0200

    Observations have comments & identification, we could post comment directly from de Pluguin

---
+++ modules/inat_obs/theme/comments.tpl.php
@@ -0,0 +1,39 @@
+<?php 
+
+
+global $base_url;
+$hour = substr($comments['updated_at'], strpos($comments['updated_at'],'T'),9);
+$hour['0'] = '';
+$data = strstr($comments['updated_at'], 'T', true).' - '.$hour;
+
+?>
+<div class='comment' id='comments-<?php print($comments['id'])?>'>  
+  <div class='user-column'> 
+  <div class='user-name'> <?php print($comments['user']['name']) ?>
+    </div>
+    <a href='<?php print($base_url)?>/inat/user/<?php print($comments['user_id'])?>'>
+     <img class='usericon' alt='' src='<?php print($comments['user']['user_icon_url'])?>'></img>
+    </a>
+  </div>
+  <div class='body-column'>
+    <div class='comment-body'> <?php print($comments['body'])?> </div>
+    <div class='comment-data'> Updated at:  <?php  print($data)?> </div>
+  </div>
+  <div class= 'ident-column'> 
+   <?php if(isset($comments['taxon'])): ?>
+  <div class='taxo'> Taxon Identification </div> 
+  <div class='info'> 
+       <div class='name-c'> <?php print($comments['taxon']['name'])?> </div>
+       <div class='name-c'> Common name:  <?php print($comments['taxon']['common_name']['name'])?> </div>
+   </div>
+   <div class='image'> 
+     <a href='<?php print($base_url)?>/inat/taxa/<?php print($comments['taxon']['id'])?>'>
+       <img class='usericon' alt='' src='<?php print($comments['taxon']['image_url'])?>'></img>
+     </a>
+   </div>
+   <?php endif; ?>
+  </div>
+
+
+
+</div>
